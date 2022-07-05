74bf1a369a0d2d867366f17b4d0b7025855cb926
ariannadepasquale <depasqualearianna@gmail.com>
2022-07-05 16:31:54 +0200

    Struttura html& intro js

---
+++ js/main.js
@@ -0,0 +1,35 @@
+//recuperiamo le informazioni agli utenti//
+let userName = document.getElementById('input-km')
+let userDistance = document.getElementById ('input-age')
+let UserAge = document.querySelector('#input-age')
+//prezzo per km//
+const pricePerKm = 0.21:
+
+//recupero il bottone che genera il biglietto//
+const ticketButton = document.getElementById('button-generate')
+
+//aggiungo event listner su evento click al bottone che genera i biglietti"
+ticketButton.addEventListener('click', function(){
+let ticketPrice = pricePerKm * userDistance.value;
+
+console.log(ticketPrice);
+console.log(userName.value);
+console.log(UserAge)
+
+if(UserAge.value == "minorenne"){
+    ticketPrice = ticketPrice * 0.2;
+    console.log("sconto del 30%")
+
+} else if(UserAge.value=="over65"){
+    ticketPrice = ticketPrice * 0.4;
+    console.log("Sconto del 40%")
+
+}else {
+ console.log("Nessuno sconto applicato")   
+}
+
+
+
+
+
+})

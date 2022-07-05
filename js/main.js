5c9e5a45354029f753b3cdd98604dad1d5626168
ariannadepasquale <depasqualearianna@gmail.com>
2022-07-05 16:41:46 +0200

    Add correzioni Js

--- js/main.js
+++ js/main.js
@@ -3,7 +3,7 @@ let userName = document.getElementById('input-km')
 let userDistance = document.getElementById ('input-age')
 let UserAge = document.querySelector('#input-age')
 //prezzo per km//
-const pricePerKm = 0.21:
+const pricePerKm = 0.21;
 
 //recupero il bottone che genera il biglietto//
 const ticketButton = document.getElementById('button-generate')
@@ -12,24 +12,24 @@ const ticketButton = document.getElementById('button-generate')
 ticketButton.addEventListener('click', function(){
 let ticketPrice = pricePerKm * userDistance.value;
 
-console.log(ticketPrice);
+console.log(ticketPrice.value);
 console.log(userName.value);
-console.log(UserAge)
+console.log(UserAge.value);
 
 if(UserAge.value == "minorenne"){
     ticketPrice = ticketPrice * 0.2;
-    console.log("sconto del 30%")
+    console.log("sconto del 20%");
 
-} else if(UserAge.value=="over65"){
+} else if(UserAge.value == "over65"){
     ticketPrice = ticketPrice * 0.4;
-    console.log("Sconto del 40%")
+    console.log("Sconto del 40%");
 
 }else {
- console.log("Nessuno sconto applicato")   
+console.log("Nessuno sconto applicato");   
 }
 
-
-
+//PREZZO DEFINITIVO//
+console.log ("ticketPrice.value");
 
 
 })
